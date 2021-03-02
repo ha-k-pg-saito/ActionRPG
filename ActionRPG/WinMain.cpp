7ef2e6c19ce1a06643221e410cbc7424c593efe0
MSI\ぺんたそ <soryuotaku@gmail.com>
2021-03-02 09:04:29 +0900

    ・不要部分削除

--- ActionRPG/WinMain.cpp
+++ ActionRPG/WinMain.cpp
@@ -37,12 +37,6 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_  HINSTANCE hPrevInstance, 
 		
 		ScreenFlip();						//画面更新
 
-
-		//clsDx();
-
-		//画面更新
-		ScreenFlip();
-
 	}
 
 	DxLib_End();							 // ＤＸライブラリ使用の終了処理
