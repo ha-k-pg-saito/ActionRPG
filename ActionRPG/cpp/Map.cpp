0a65ec4503bd25bcae899085907a313df9d4d3e0
MSI\ぺんたそ <soryuotaku@gmail.com>
2021-05-20 23:40:49 +0900

    ・マップのモデル更新 ・レイとマップのあたり判定をマップに移行

--- ActionRPG/cpp/Map.cpp
+++ ActionRPG/cpp/Map.cpp
@@ -1,6 +1,6 @@
 #include"../h/Map.h"
 
-void Map::Init(int maphandle,int grhandle,int lastmaphandle,int lastgrhandle)
+void Map::Init(int maphandle,int grhandle)
 {
 	
 #pragma region マップモデル読み込み
@@ -8,28 +8,21 @@ void Map::Init(int maphandle,int grhandle,int lastmaphandle,int lastgrhandle)
 	m_MapHandle = maphandle;
 	//初期マップテクスチャ
 	m_GrHandle[0] = grhandle;
-	m_GrHandle[1] =  LoadGraph("Tex/Stage/grass.jpg");
-	m_GrHandle[2] =  LoadGraph("Tex/Stage/ground.jpg");
-	m_GrHandle[3] =  LoadGraph("Tex/Stage/bridge.jpg");
-	m_GrHandle[4] =  LoadGraph("Tex/Stage/water.jpg");
-	m_GrHandle[5] =  LoadGraph("Tex/Stage/wood.jpg");
-	m_GrHandle[6] =  LoadGraph("Tex/Stage/bark.jpg");
-	//ボス戦のマップモデル
-	m_LastMHandle = lastmaphandle;
-	//ボス戦のマップテクスチャ
-	m_LastMGrHandle[0] = lastgrhandle;
-	m_LastMGrHandle[1] = LoadGraph("Tex/Stage/wood kawa.jpg");
-	m_LastMGrHandle[2] = LoadGraph("Tex/Stage/wood yuka.jpg");
-	m_LastMGrHandle[3] = LoadGraph("Tex/Stage/wood kabe.jpg");
+	m_GrHandle[1] =  LoadGraph("Tex/Stage/ground.jpg");
+	m_GrHandle[2] =  LoadGraph("Tex/Stage/bark.jpg");
+	m_GrHandle[3] =  LoadGraph("Tex/Stage/wood.jpg");
+	m_GrHandle[4] =  LoadGraph("Tex/Stage/wood kawa.jpg");
+	m_GrHandle[5] =  LoadGraph("Tex/Stage/wood yuka.jpg");
+	m_GrHandle[6] =  LoadGraph("Tex/Stage/wood kabe.jpg");
+	m_GrHandle[7] =  LoadGraph("Tex/Stage/grass.jpg");
+	m_GrHandle[8] =  LoadGraph("Tex/Stage/bridge.jpg");
+	m_GrHandle[9] =  LoadGraph("Tex/Stage/water.jpg");
+
 #pragma endregion
 	//モデルに貼るテクスチャの数分だけfor文を回す
-	for (int i = 0; i <7 ; i++)
+	for (int i = 0; i <10 ; i++)
 	{
 		MV1SetTextureGraphHandle(m_MapHandle, i, m_GrHandle[i], FALSE);
-	}
-	for (int i = 0; i < 4; i++)
-	{
-		MV1SetTextureGraphHandle(m_LastMHandle, i, m_LastMGrHandle[i], FALSE);
 	}
 }
 
@@ -39,4 +32,31 @@ void Map::Draw()
 	MV1DrawModel(m_MapHandle);
 	//MV1SetPosition(m_LastMHandle, m_Pos);
 	//MV1DrawModel(m_LastMHandle);
+}
+
+bool Map::CollisionToModel(VECTOR startpos,VECTOR endpos)
+{
+	//モデル全体のコリジョン情報構築
+	MV1SetupCollInfo(m_MapHandle, -1, 8, 8, 8);
+	//0番目のフレームのコリジョン情報を更新する
+	MV1RefreshCollInfo(m_MapHandle, 0);
+	//-1番フレームとレイとのあたり判定
+	//m_Posは始点・m_Lineは終点
+	HitPoly = MV1CollCheck_Line(m_MapHandle, -1, startpos, endpos);
+
+	//当たったならその位置をレイの終点とする
+	if (HitPoly.HitFlag == 1)
+	{
+		endpos = HitPoly.HitPosition;
+	}
+	if (HitPoly.HitFlag == 1)
+	{
+		// 当たったかどうかを表示する
+		DrawFormatString(1000, 200, GetColor(255, 255, 255), "HIT:%d", HitPoly.HitFlag);
+	}
+	else
+	{
+		DrawString(1000, 200, "当たってない", GetColor(255, 255, 255));
+	}
+		return false;
 }
--- ActionRPG/h/Player.h
+++ ActionRPG/h/Player.h
@@ -24,7 +24,7 @@ public:
 		m_AnimHandle{ 0 },
 		m_Direction{0}
 	{
-		m_Speed = (20.f);
+		m_Speed = (100.f);
 		m_Hp = (0.f);
 		m_HitCounter = (0);
 	}
@@ -52,7 +52,6 @@ public:
 private:
 	void Rotate();
 	void Move();
-	void CollisionToMap(Map* map);
 	void Damage();
 
 private:
@@ -60,9 +59,6 @@ private:
 	int m_ModelHandle;
 	//3Dモデルに貼るテクスチャ保存変数
 	int m_GrHandle[8];
-
-	//デバッグ用
-	bool push = false;
 
 	//アニメーションに使用する変数
 	int   m_AnimHandle[ANIM_NUM];
@@ -77,11 +73,12 @@ private:
 
 	//レイの描画に使う変数
 	VECTOR m_Line;
-	//DxLibにあるあたり判定用の戻り値+変数
-	MV1_COLL_RESULT_POLY HitPoly;
 
 	//当たった回数を保存する変数
 	int m_HitCounter;
+
+	//マップクラスのインスタンス化
+	Map map;
 };
 
 
